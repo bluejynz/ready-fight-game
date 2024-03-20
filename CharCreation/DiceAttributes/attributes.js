696ce2c0605091cebd1c7ae22de9ad6f56590b89
bluejynz <dany_falk@hotmai.com>
2024-03-20 18:01:08 -0300

    roll btn

--- CharCreation/DiceAttributes/attributes.js
+++ CharCreation/DiceAttributes/attributes.js
@@ -91,19 +91,40 @@ const dice = {
         </div>`,
 };
 
+let diceAttributeRolls = [];
+
 function roll(btn) {
-    btn.remove();
-    let rngRolls = rollDice(6, 6);
+    btn.textContent = `Roll dice x${5 - diceAttributeRolls.length}`;
+    let rngRolls = rollDice(5, 6);
+    diceAttributeRolls.push(sumResultsNoMinor(rngRolls));
+    saveDiceResults(diceAttributeRolls);
     createDiceElements(rngRolls);
+    if (diceAttributeRolls.length >= 6) {
+        btn.remove();
+    }
+}
+
+function sumResultsNoMinor(results) {
+    let sum = 0;
+    for (i = 0; i < results.length; i++) {
+        if (getIndexOfMinorValue(results) != i) {
+            sum += results[i];
+        }
+    }
+    return sum;
 }
 
 function createDiceElements(rngRolls) {
     let diceContainer = document.getElementById("dice-container");
-    let h2Info = document.createElement("h2");
-    h2Info.textContent = "Suas rolagens foram:";
-    diceContainer.before(h2Info);
+    diceContainer.innerHTML = "";
+    if (!document.getElementById("h2-dice-info")) {
+        let h2Info = document.createElement("h2");
+        h2Info.id = "h2-dice-info";
+        h2Info.textContent = "Suas rolagens foram:";
+        diceContainer.before(h2Info);
+    }
     let minorIndex = getIndexOfMinorValue(rngRolls);
-    rngRolls.forEach(r => {
+    rngRolls.forEach((r) => {
         diceContainer.innerHTML += dice[r];
     });
     let minorDieElement = diceContainer.children[minorIndex];
@@ -111,17 +132,25 @@ function createDiceElements(rngRolls) {
     createAttributesAllocationElements();
 }
 
+function saveDiceResults(values) {
+    console.log("Array stringify", JSON.stringify(values));
+    const diceResults = JSON.stringify(values);
+    localStorage.setItem('diceResults', diceResults);
+}
+
 function createAttributesAllocationElements() {
     // TODO:
     // criar mais um main-container no html escondido
     // ao chamar essa função mostrar o container
+    let allocationContainer = document.getElementById("allocation-container");
+    allocationContainer.classList.remove("hidden");
     // colocar todos os atributos
     // ao lado um select com os valores rolados pelos dados
     // quando selecionar um, esse mesmo não pode ser selecionado em outro
     // fazer os atributos baseados em d&d 5e
 }
 
-function rollDice(size, qty) {
+function rollDice(qty, size) {
     let rngRolls = [];
     for (let i = 1; i <= qty; i++) {
         rngRolls.push(randomIntFromInterval(1, size));
@@ -133,14 +162,14 @@ function getIndexOfMinorValue(arr) {
     let minorValue;
     let minorIndex;
     for (let i = 0; i < arr.length; i++) {
-        if(!minorValue) {
+        if (!minorValue) {
             minorValue = arr[i];
             minorIndex = i;
-        } else if(arr[i] < minorValue) {
+        } else if (arr[i] < minorValue) {
             minorValue = arr[i];
             minorIndex = i;
         }
-        if(minorValue && minorValue == 1) {
+        if (minorValue && minorValue == 1) {
             break;
         }
     }
